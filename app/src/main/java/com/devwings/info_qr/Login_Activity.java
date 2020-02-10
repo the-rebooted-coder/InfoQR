3a01a0ae689e1b4763d2ef54e0317cfa4dbfc7b0
the-rebooted-coder <spandansaxena@gmail.com>
2020-02-11 00:38:37 +0530

    Added Google Sign In Button

--- app/src/main/java/com/devwings/info_qr/Login_Activity.java
+++ app/src/main/java/com/devwings/info_qr/Login_Activity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login_Activity extends AppCompatActivity {
 
     @Override
