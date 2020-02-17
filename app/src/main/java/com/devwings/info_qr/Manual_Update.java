cc7eeb2760a461be99309a1e967ee64ac84a6baa
spandan saxena <spandansaxena@gmail.com>
2020-02-18 02:46:31 +0530

    Added First Update to Google FireStore Database Update
    Added The Functionality to Add Data to A cloud based database with the current selected user account. Haven't Tried in actual build, however no errors as of yet!

--- app/src/main/java/com/devwings/info_qr/Manual_Update.java
+++ app/src/main/java/com/devwings/info_qr/Manual_Update.java
@@ -1,17 +1,29 @@
 package com.devwings.info_qr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Manual_Update extends AppCompatActivity {
+
+    public static final String AUTHOR_KEY = "author";
+    public static final String QUOTE_KEY = "quote";
+    public static final String TAG = "Inspiring Quote";
+
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("sampleData/inspiration");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +31,29 @@ public class Manual_Update extends AppCompatActivity {
         setContentView(R.layout.activity_manual_update);
     }
 
-    public void saveQuote(View view) {
-        EditText quoteView = (EditText) findViewById(R.id.editTextQuote);
-        EditText authorView = (EditText) findViewById(R.id.editTextAuthor);
+    public void saveQuote (View view) {
+        TextView quoteView = (TextView) findViewById(R.id.editTextQuote);
+        TextView authorView = (TextView) findViewById(R.id.editTextAuthor);
         String quoteText = quoteView.getText().toString();
         String authorText = quoteView.getText().toString();
 
         if (quoteText.isEmpty() || authorText.isEmpty()){
             return;
         }
-        Map<String. Object> dataToSave = new 
+        Map<String, Object> dataToSave = new HashMap<String, Object>();
+        dataToSave.put(QUOTE_KEY, quoteText);
+        dataToSave.put(AUTHOR_KEY, authorText);
+        mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "Document has Been Saved!");
 
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Document was Not Saved" ,e);
+            }
+        });
     }
 }
--- app/src/main/java/com/devwings/info_qr/Splash_Screen.java
+++ app/src/main/java/com/devwings/info_qr/Splash_Screen.java
@@ -18,7 +18,7 @@ public class Splash_Screen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i=new Intent(Splash_Screen.this,Login_Activity.class);
+                Intent i=new Intent(Splash_Screen.this,Scan_Activity.class);
                 startActivity(i);
                 finish();
             }
