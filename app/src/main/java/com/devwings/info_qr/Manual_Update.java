76ff0cca0839d7524654bb9cf0fb93f86014ba7f
spandan saxena <spandansaxena@gmail.com>
2020-02-18 01:58:30 +0530

    Added FireStore Class

--- app/src/main/java/com/devwings/info_qr/Manual_Update.java
+++ app/src/main/java/com/devwings/info_qr/Manual_Update.java
@@ -3,15 +3,32 @@ package com.devwings.info_qr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
 
 public class Manual_Update extends AppCompatActivity {
-
-    private static final String EQUIP_ID = "EquipmentID";
-    private static final String EQUIP_ID = "EquipmentID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_update);
+    }
+
+    public void saveQuote(View view) {
+        EditText quoteView = (EditText) findViewById(R.id.editTextQuote);
+        EditText authorView = (EditText) findViewById(R.id.editTextAuthor);
+        String quoteText = quoteView.getText().toString();
+        String authorText = quoteView.getText().toString();
+
+        if (quoteText.isEmpty() || authorText.isEmpty()){
+            return;
+        }
+        Map<String. Object> dataToSave = new 
+
     }
 }
