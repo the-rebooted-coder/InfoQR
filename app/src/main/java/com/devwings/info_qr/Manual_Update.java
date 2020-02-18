4a1e81db7047843ed2bee6a39df7d93ccaa2ce5b
spandan saxena <spandansaxena@gmail.com>
2020-02-19 01:30:44 +0530

    Added Database Fetch Capabilty to the App
    The Data could be fetched using the button in the Manual Update Button!
    
    Can Fetch from Origin...NO ERR

--- app/src/main/java/com/devwings/info_qr/Manual_Update.java
+++ app/src/main/java/com/devwings/info_qr/Manual_Update.java
@@ -8,51 +8,74 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Manual_Update extends AppCompatActivity {
 
-    public static final String AUTHOR_KEY = "author";
-    public static final String QUOTE_KEY = "quote";
+    public static final String EQUIP_ID = "id";
+    public static final String EQUIP_NAME = "name";
     public static final String TAG = "Inspiring Quote";
 
-    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("Equipments/inspiration");
+    TextView mQuoteTextView;
+
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("Equipment/information");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_update);
+        mQuoteTextView = (TextView) findViewById(R.id.textViewInspiring);
+    }
+
+    public void fetchQuote(View view){
+
+        mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+
+                    String equip_id = documentSnapshot.getString(EQUIP_ID);
+                    String equip_name = documentSnapshot.getString(EQUIP_NAME);
+                    mQuoteTextView.setText("\"ID:- " +System.getProperty("line.separator")+ equip_id + System.getProperty("line.separator")+"\" Equip Name:- "+System.getProperty("line.separator") + equip_name);
+                    Toast.makeText(Manual_Update.this, "Data Fetch Successful!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
     public void saveQuote (View view) {
-        EditText quoteView = (EditText) findViewById(R.id.editTextQuote);
-        EditText authorView = (EditText) findViewById(R.id.editTextAuthor);
-        String quoteText = quoteView.getText().toString();
-        String authorText = authorView.getText().toString();
+        EditText equipID_View = (EditText) findViewById(R.id.editEquipID);
+        EditText equipNAMEView = (EditText) findViewById(R.id.editEquipName);
+        String quoteText = equipID_View.getText().toString();
+        String authorText = equipNAMEView.getText().toString();
 
         if (quoteText.isEmpty() || authorText.isEmpty()){
             return;
         }
         Map<String, Object> dataToSave = new HashMap<String, Object>();
-        dataToSave.put(QUOTE_KEY, quoteText);
-        dataToSave.put(AUTHOR_KEY, authorText);
+        dataToSave.put(EQUIP_ID, quoteText);
+        dataToSave.put(EQUIP_NAME, authorText);
         mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
-                Log.d(TAG, "Document Has Been Saved!");
+                Toast.makeText(Manual_Update.this, "Successfully Added to Database", Toast.LENGTH_SHORT).show();
 
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Log.w(TAG, "Document was Not Saved" ,e);
+                Toast.makeText(Manual_Update.this, "Failed to Add Data To Database", Toast.LENGTH_SHORT).show();
             }
         });
     }
