6c05a0a2c052bc405866f229a4e3bcc9781ff8c6
the-coded-bit <theburningdesiresince2002@gmail.com>
2020-02-24 23:51:09 +0530

    Update User_Info.java

--- app/src/main/java/com/devwings/info_qr/User_Info.java
+++ app/src/main/java/com/devwings/info_qr/User_Info.java
@@ -10,5 +10,6 @@ public class User_Info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_nfo);
+        /*this is user info*/
     }
 }
