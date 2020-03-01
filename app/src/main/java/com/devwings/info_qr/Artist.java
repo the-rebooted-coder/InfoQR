5cebb6550d51fd54aeb3d97d4b1c04cb6f62ffad
spandan saxena <spandansaxena@gmail.com>
2020-03-01 20:38:46 +0530

    Created a Class Artist and Modified the orignal contents of Manual Update Activity to comply with the Realtime Database. Multiple Entries are now started to show up on the server side and is working well as of now!

---
+++ app/src/main/java/com/devwings/info_qr/Artist.java
@@ -0,0 +1,28 @@
+package com.devwings.info_qr;
+
+public class Artist {
+    String artistId;
+    String artistName;
+    String artistGenre;
+
+    public Artist() {
+    }
+
+    public Artist(String artistId, String artistName, String artistGenre) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+        this.artistGenre = artistGenre;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getArtistGenre() {
+        return artistGenre;
+    }
+}
--- app/src/main/java/com/devwings/info_qr/Manual_Update.java
+++ app/src/main/java/com/devwings/info_qr/Manual_Update.java
@@ -1,9 +1,12 @@
 package com.devwings.info_qr;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +16,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -21,76 +26,48 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Manual_Update extends AppCompatActivity {
-    public static final String EQUIP_ID = "id";
-    public static final String EQUIP_NAME = "name";
-    public static final String DATE = "date";
-    TextView mQuoteTextView;
 
-    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("Equipment/information");
+    EditText editTextName;
+    Button buttonAdd;
+    Spinner spinnerGenres;
+
+    DatabaseReference databaseArtists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_manual_update);
-        mQuoteTextView = (TextView) findViewById(R.id.textViewInspiring);
-    }
+        databaseArtists = FirebaseDatabase.getInstance().getReference("artists");
 
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        buttonAdd = (Button) findViewById(R.id.buttonAddArtist);
+        spinnerGenres = (Spinner) findViewById(R.id.spinnerGenres);
 
-    public void fetchQuote(final View view){
-
-        mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                if (documentSnapshot.exists()) {
-
-                    String equip_id = documentSnapshot.getString(EQUIP_ID);
-                    String equip_name = documentSnapshot.getString(EQUIP_NAME);
-                    String equip_date = documentSnapshot.getString(DATE);
-                    mQuoteTextView.setText("\"ID:- " +System.getProperty("line.separator")+ equip_id + System.getProperty("line.separator")+"\" Equip Name:- "+System.getProperty("line.separator") + equip_name + System.getProperty("line.separator")+"\" Last Service Date:- "+System.getProperty("line.separator") + equip_date);
-                    Snackbar snackbar = Snackbar
-                            .make(view, "Secure 128 BIT Connection to Server Established", Snackbar.LENGTH_INDEFINITE);
-                    snackbar.show();
-                }
+            public void onClick(View view) {
+                addArtist();
             }
         });
-
     }
+    private void addArtist(){
+        String name = editTextName.getText().toString().trim();
+        String genre  = spinnerGenres.getSelectedItem().toString();
 
-    public void saveQuote (final View view) {
-        EditText equipID_View = (EditText) findViewById(R.id.editEquipID);
-        EditText equipNAMEView = (EditText) findViewById(R.id.editEquipName);
-        EditText equipDATEView = (EditText) findViewById(R.id.editEquipService);
-        String quoteText = equipID_View.getText().toString();
-        String authorText = equipNAMEView.getText().toString();
-        String detailText = equipDATEView.getText().toString();
-
-        if (quoteText.isEmpty() || authorText.isEmpty()){
-            return;
+        if (!TextUtils.isEmpty(name))
+        {
+            String id = databaseArtists.push().getKey();
+            Artist artist = new Artist(id, name, genre);
+            databaseArtists.child(id).setValue(artist);
+            Toast.makeText(this, "Artist Added", Toast.LENGTH_LONG).show();
         }
-        Map<String, Object> dataToSave = new HashMap<String, Object>();
-        dataToSave.put(EQUIP_ID, quoteText);
-        dataToSave.put(EQUIP_NAME, authorText);
-        dataToSave.put(DATE, detailText);
-        mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Toast.makeText(Manual_Update.this, "Added!", Toast.LENGTH_SHORT).show();
-                Snackbar snackbar = Snackbar
-                        .make(view, "Secure 128 BIT Connection to Server Established", Snackbar.LENGTH_INDEFINITE);
-                snackbar.show();
+        else{
+            Toast.makeText(this, "You should enter a name", Toast.LENGTH_LONG).show();
+        }
 
-
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(Manual_Update.this, "Failed to Add Data To Database", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
-
 
 
 /*
