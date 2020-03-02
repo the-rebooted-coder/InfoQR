12d37059f16ef347f55a67b486dc8376e6a91fac
spandan saxena <spandansaxena@gmail.com>
2020-03-03 02:44:15 +0530

    Added code for Database Fetch

--- app/src/main/java/com/devwings/info_qr/ArtistList.java
+++ app/src/main/java/com/devwings/info_qr/ArtistList.java
@@ -3,7 +3,16 @@ package com.devwings.info_qr;
 
 
 import android.app.Activity;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import java.util.List;
 
 
@@ -20,7 +29,7 @@ public class ArtistList extends ArrayAdapter<Artist> {
 
     public ArtistList(Activity context, List<Artist> artistList){
 
-        super(context, R.layout.list_layout);
+        super(context, R.layout.list_layout, artistList);
 
         this.context = context;
 
@@ -28,6 +37,21 @@ public class ArtistList extends ArrayAdapter<Artist> {
 
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View listViewItem = inflater.inflate(R.layout.list_layout, null, true);
+        TextView textViewName = (TextView) listViewItem.findViewById(R.id.textViewName);
+        TextView textViewGenre = (TextView) listViewItem.findViewById(R.id.textViewGenre);
+
+        Artist artist = artistList.get(position);
+        textViewName.setText(artist.getArtistName());
+        textViewGenre.setText(artist.getArtistGenre());
+
+        return  listViewItem;
+    }
 }
 
 
